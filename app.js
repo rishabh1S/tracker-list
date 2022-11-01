7d9cf111c53d25a037cc3fe43688e8bd805ac8fd
Rishabh Singh <89694788+SinghRishabh19@users.noreply.github.com>
2022-11-01 10:44:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import date from "./date.js";
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+   
+    let day = date.getDate();
+    res.render("list", { listTitle: day, newListItems: items});
+});
+
+app.post("/", function (req, res) {  
+    let item = req.body.newItem; 
+
+    if(req.body.list==="Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else{
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+ 
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log("Server started at port 3001");
+});
