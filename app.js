080aaa0e50cb30443af524fc1dd766866bae6036
Rishabh Singh <89694788+SinghRishabh19@users.noreply.github.com>
2022-11-01 11:00:33 +0530

    check commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
    
-    let day = date.getDate();
+    let day = date.getDay();
     res.render("list", { listTitle: day, newListItems: items});
 });
 
